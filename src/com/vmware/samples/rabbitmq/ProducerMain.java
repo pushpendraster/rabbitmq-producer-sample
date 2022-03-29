5a65a9dbfb2ca8f6c8ba49b1b0aa6ecbc59cb86e
Pushpendra Singh Baghel <pbaghel@vmware.com>
2022-03-29 20:44:08 +0530

    Added Fisrt Draft

---
+++ src/com/vmware/samples/rabbitmq/ProducerMain.java
@@ -0,0 +1,25 @@
+package com.vmware.samples.rabbitmq;
+
+import java.nio.charset.StandardCharsets;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+public class ProducerMain {
+
+	private final static String QUEUE_NAME = "hello";
+
+	public static void main(String[] argv) throws Exception {
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setHost("20.25.90.107");
+		factory.setPort(8080);
+		try (Connection connection = factory.newConnection(); Channel channel = connection.createChannel()) {
+			channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+			String message = "Hello World!";
+			channel.basicPublish("", QUEUE_NAME, null, message.getBytes(StandardCharsets.UTF_8));
+			System.out.println(" [x] Sent '" + message + "'");
+		}
+	}
+
+}
